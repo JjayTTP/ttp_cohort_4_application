a269e4f2c1f5379a8eee6b4d6d3174d1176d74cd
shawns21 <shawnsinanan21@gmail.com>
2023-04-22 15:09:40 -0400

    added linkedin box

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ const looker = new IntersectionObserver((entries) => {
 
 const hiddenElems = document.querySelectorAll('.hidden');
 hiddenElems.forEach((el) => looker.observe(el));
+
+const icons = document.querySelectorAll('.portLinks');
+
+icons.forEach(icon => {
+
+    icon.addEventListener('click', () => {
+        window.open(icon.dataset.url, target="_blank");
+    })
+
+    icon.style.cursor = "pointer";
+
+})
