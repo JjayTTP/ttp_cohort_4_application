88260ee0285dcbd75f66a6f8cdb056b340c9e93c
Scam Likely <scamlikely@Scams-MacBook-Pro.local>
2023-04-23 22:58:19 -0400

    finish

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ const mouseCircleFn = (x, y) => {
 }
 
 // animated circles
-
 const circles = document.querySelectorAll('.circle');
 const mainImg = document.querySelector(".main-circle img");
 
@@ -24,8 +23,8 @@ let mX = 0;
 let mY = 0;
 const z = 50;
 
+// shifts the circles in the opposite direction based on the top and left values. top and left are adjacent
 const animateCircles = (evt, x, y) => {
-
     if(x < mX) {
         circles.forEach(circle => {
             circle.style.left = `${z}px`;
@@ -49,13 +48,8 @@ const animateCircles = (evt, x, y) => {
         })
         mainImg.style.top = `-${z}px`
     }
-
-    
-    
     mX = evt.clientX;
     mY = evt.clientY;
-
-
 }
 
 // end of animated circles
@@ -74,9 +68,32 @@ document.body.addEventListener("mouseleave", () => {
 })
 // end of mouse circle
 
+// navigation
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
+
+document.addEventListener('scroll', () => {
+    menuIcon.classList.add('show-menu-icon');
+    navbar.classList.add('hide-navbar');
+
+    if(window.scrollY === 0) {
+        menuIcon.classList.remove("show-menu-icon");
+        navbar.classList.remove("hide-navbar");
+    }
+})
+
+// listener for the menu icon to open 
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.remove("show-menu-icon");
+    navbar.classList.remove("hide-navbar");
+})
+
+// end of navigation
+
+
 // about me text
 const aboutMeText = document.querySelector('.about-me-text');
-const aboutMeTextContent = "I am a web developer based in NY. A student for life who runs on caesar salads. Contact me. :)";
+const aboutMeTextContent = "I am a web developer based in NY. A student for life who runs on caesar salads and good conversation. Get to know me. :)";
 
  Array.from(aboutMeTextContent).forEach((char) => {
     const span = document.createElement('span');
@@ -88,3 +105,87 @@ const aboutMeTextContent = "I am a web developer based in NY. A student for life
     })
  });
 // end of about me text
+
+
+// section 4
+
+// form 
+const formHeading = document.querySelector('.form-heading');
+const formInputs = document.querySelectorAll('.contact-form-input');
+
+formInputs.forEach(input => {
+    input.addEventListener('focus', () => {
+        formHeading.style.opacity = "0";
+        setTimeout(() => {
+            formHeading.textContent = `Your ${input.placeholder}`
+            formHeading.style.opacity = "1"
+        }, 300)
+    })
+
+    input.addEventListener('blur', () => {
+        formHeading.style.opacity = "0";
+        setTimeout(() => {
+            formHeading.textContent = "Lets Talk";
+            formHeading.style.opacity = "1"
+        }, 300)
+    })
+})
+
+// form validation 
+const form = document.querySelector('.contact-form');
+const username = document.getElementById('name')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+const errorMessages = document.querySelectorAll('.message')
+
+const error = (inputElement, message) => {
+    inputElement.nextElementSibling.classList.add("error")
+    inputElement.nextElementSibling.textContent = message   
+}
+
+// removes error message if field is populated after submission
+const success = (input) => {
+    input.nextElementSibling.classList.remove("error")
+}
+
+// checks if the field is empty
+const checkRequiredFields = (inputArr) => {
+    inputArr.forEach(input => {
+        if (input.value.trim() === "") {
+            error(input, `${input.id} is required`);
+        } 
+    })
+}
+
+// checks if the input length is smaller than the minimum number
+const checkLength = (input, min) => {
+    if (input.value.trim().length < min) {
+        error(input, `${input.id} must be at least ${min} characters`);
+    } else {
+        success(input);
+    }
+}
+
+const checkEmail = (input) => {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(regEx.test(input.value.trim())) {
+        success(input)
+    } else {
+        error(input, `Email is not valid`)
+    }
+}
+
+form.addEventListener("submit", evt => {
+    evt.preventDefault();
+
+    checkLength(username, 3)
+    checkLength(subject, 5)
+    checkLength(message, 10)
+    checkEmail(email);
+    checkRequiredFields([username, email,subject, message]);
+})
+
+// end of form validation
+
+// end of section 4
