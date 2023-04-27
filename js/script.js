8a52d10b932a4ef197e4821cc75d58561709f285
shawns21 <shawnsinanan21@gmail.com>
2023-04-27 00:37:58 -0400

    Final changes, added movie poster slideshow

--- js/script.js
+++ js/script.js
@@ -24,3 +24,25 @@ icons.forEach(icon => {
     icon.style.cursor = "pointer";
 
 })
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
